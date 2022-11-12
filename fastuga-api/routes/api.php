695d32f88dc4dea9e4cb07c35d19f7d33e5efa39
joseareia <jose.apareia@gmail.com>
2022-11-12 16:48:39 +0000

    add(routes): api resource generic routes

--- fastuga-api/routes/api.php
+++ fastuga-api/routes/api.php
@@ -2,18 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\OrderItemController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('orders', OrderController::class);
+Route::resource('users', UserController::class);
+Route::resource('products', ProductController::class);
+Route::resource('customers', CustomerController::class);
+Route::resource('order-items', OrderItemController::class);
