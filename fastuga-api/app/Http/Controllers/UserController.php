478f31ddebeda504011ec248bf9072a7ec7bc9e2
joseareia <jose.apareia@gmail.com>
2022-11-20 08:34:25 +0000

    add: crud operations for user + customer -- incomplete

--- fastuga-api/app/Http/Controllers/UserController.php
+++ fastuga-api/app/Http/Controllers/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Resources\UserResource;
 use App\Models\User;
+use App\Http\Resources\UserResource;
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -13,14 +16,25 @@ class UserController extends Controller
         return UserResource::collection(User::all());
     }
 
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        /* --- Authorization --- */
+        if ($request['type'] != 'C' && Auth()->user()->type() != "EM") { abort(403); }
+
+        $new_user = DB::transaction(function () use ($request) : User {
+            $user = User::create($request->validated());
+            $password_hashed = Hash::make($request->input('password'));
+            $user->password = $password_hashed;
+            $user->save();
+            return $user;
+        });
+
+        return new UserResource($new_user);
     }
 
     public function show($id)
     {
-        return UserResource::collection(User::where('id', $id)->get());
+        return new UserResource(User::findOrFail($id));
     }
 
     public function update(Request $request, $id)
@@ -30,6 +44,8 @@ class UserController extends Controller
 
     public function destroy($id)
     {
-        //
+        $user = new UserResource(User::findOrFail($id));
+        // if ($user->customer) { $user->customer->forceDelete(); } --> NONSENSE?
+        return $deleted = $user->delete();
     }
 }
