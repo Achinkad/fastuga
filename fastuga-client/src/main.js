97f4c446d8639c6156cddc2f61bd8ecd6f2f67be
Tiago Pereira <2212536@my.ipleiria.pt>
2022-11-21 20:01:13 +0000

    Template

--- fastuga-client/src/main.js
+++ fastuga-client/src/main.js
@@ -3,7 +3,9 @@ import { createPinia } from 'pinia'
 import axios from 'axios'
 import App from './App.vue'
 import router from './router'
-
+import Toaster from "@meforma/vue-toaster";
+import FieldErrorMessage from './components/global/FieldErrorMessage.vue'
+import ConfirmationDialog from './components/global/ConfirmationDialog.vue'
 //import './assets/main.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
@@ -19,7 +21,15 @@ app.provide('axios', axios.create({
     },
 }))
 app.provide('serverBaseUrl', serverBaseUrl)
+app.use(Toaster, {
+    // Global/Default options
+    position: 'top',
+    timeout: 3000,
+    pauseOnHover: true
+})
+app.provide('toast', app.config.globalProperties.$toast);
 app.use(createPinia())
 app.use(router)
-
+app.component('FieldErrorMessage', FieldErrorMessage)
+app.component('ConfirmationDialog', ConfirmationDialog)
 app.mount('#app')
