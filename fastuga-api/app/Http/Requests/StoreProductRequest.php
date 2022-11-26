99edb700fa68594320d001a3a0c8fc10d9c903be
joseareia <jose.apareia@gmail.com>
2022-11-26 07:46:02 +0000

    edit: product controller ok (no authorization)

---
+++ fastuga-api/app/Http/Requests/StoreProductRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProductRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:products|min:3|max:255',
+            'type' => 'required|in:hot dish,cold dish,drink,dessert',
+            'description' => 'required|max:255',
+            'photo_url' => 'required',
+            'price' => 'required',
+            'custom' => 'nullable'
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'data' => $validator->errors()
+        ], 400));
+    }
+}
