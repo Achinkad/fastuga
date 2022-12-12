041915c4c6ba4bcd9b20c3841d855397d62b5010
Tiago Pereira <2212536@my.ipleiria.pt>
2022-12-12 11:48:59 +0000

    Preparing orders still needs to update auto

--- fastuga-client/src/layout/Sidebar.vue
+++ fastuga-client/src/layout/Sidebar.vue
@@ -103,7 +103,7 @@ onMounted(() => {
 
 
                 <ul class="nav flex-column">
-                    <li class="nav-item nav-item-title">Orders</li>
+                    <li class="nav-item nav-item-title">In preparation</li>
 
                     <li class="nav-item" v-for="order in orders" :key="order.id">
                         <div v-if="order.status == 'P'">
