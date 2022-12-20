37957270cac3e54a7d6ffa9b7b3dc2bd78ad6d6b
anaritaortigoso <100567335+anaritaortigoso@users.noreply.github.com>
2022-12-20 11:02:58 +0000

    Add: Authorizartions for chefs (please report bugs)

--- api/app/Http/Kernel.php
+++ api/app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.manager' => \App\Http\Middleware\ManagerAuthorization::class,
+        'auth.chef' => \App\Http\Middleware\ChefAuthorization::class,
     ];
 }
