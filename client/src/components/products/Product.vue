99b43468b6124148f4a00986fa447791443a6344
Tiago Pereira <2212536@my.ipleiria.pt>
2022-12-22 12:20:27 +0000

    cleaned code

--- client/src/components/products/Product.vue
+++ client/src/components/products/Product.vue
@@ -2,15 +2,15 @@
 import { ref, watch, inject } from 'vue'
 import ProductDetail from "./ProductDetail.vue"
 import { useRouter } from 'vue-router'
+
 import axios from 'axios'
 
 
 const router = useRouter()
 const toast = inject('toast')
 const serverBaseUrl = inject("serverBaseUrl")
+
 axios.defaults.headers.common.Authorization = "Bearer " + sessionStorage.token
-const product = ref(newProduct())
-const errors = ref(null)
 
 const props = defineProps({
     id: {
@@ -39,48 +39,48 @@ const loadProduct = (id) => {
 
     } else {
         axios.get(serverBaseUrl + '/api/products/' + id)
-            .then((response) => {
-                product.value = response.data.data
-                originalValueStr = dataAsString()
-            })
-            .catch((error) => {
-                console.log(error)
-            })
+        .then((response) => {
+            product.value = response.data.data
+            originalValueStr = dataAsString()
+        })
+        .catch((error) => {
+            console.log(error)
+        })
     }
 }
 
 const save = (product_values) => {
     axios.post(serverBaseUrl + '/api/products/' + props.id, product_values)
-        .then((response) => {
-            toast.success("Product updated successfuly!")
-            router.push({ name: 'Products' })
-        })
-        .catch((error) => {
-            if (error.response.status == 422) {
-                toast.error('Product #' + props.id + ' was not updated due to validation errors!')
-                errors.value = error.response.data.data
-            } else {
-                toast.error('Product #' + props.id + ' was not updated due to unknown server error!')
-            }
-        })
+    .then((response) => {
+        toast.success("Product updated successfuly!")
+        router.push({ name: 'Products' })
+    })
+    .catch((error) => {
+        if (error.response.status == 422) {
+            toast.error('Product #' + props.id + ' was not updated due to validation errors!')
+            errors.value = error.response.data.data
+        } else {
+            toast.error('Product #' + props.id + ' was not updated due to unknown server error!')
+        }
+    })
 }
 
 const add = (product_values) => {
     axios.post(serverBaseUrl + '/api/products', product_values)
-        .then((response) => {
-            toast.success("Product added successfuly")
-            router.push({ name: 'Products' })
-        })
-        .catch((error) => {
-            if (error.response.status == 422) {
+    .then((response) => {
+        toast.success("Product added successfuly")
+        router.push({ name: 'Products' })
+    })
+    .catch((error) => {
+        if (error.response.status == 422) {
 
-                toast.error('Couldn\'t add the product due to validation errors!')
-                errors.value = error.response.data.data
-                // console.log(errors.value)
-            } else {
-                toast.error('Couldn\'t add the product due to unknown server error!')
-            }
-        })
+            toast.error('Couldn\'t add the product due to validation errors!')
+            errors.value = error.response.data.data
+            // console.log(errors.value)
+        } else {
+            toast.error('Couldn\'t add the product due to unknown server error!')
+        }
+    })
 }
 
 const cancel = () => {
@@ -96,6 +96,8 @@ const dataAsString = () => {
 
 
 
+const product = ref(newProduct())
+const errors = ref(null)
 
 
 watch(
@@ -110,5 +112,7 @@ watch(
 </script>
 
 <template>
+
+
     <product-detail :product="product" :errors="errors" @save="save" @cancel="cancel" @add="add"></product-detail>
 </template>
