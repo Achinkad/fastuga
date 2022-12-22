27909344e7f4177c0421763fc2bf026572936869
anaritaortigoso <100567335+anaritaortigoso@users.noreply.github.com>
2022-12-22 15:01:26 +0000

    Error messages

--- client/src/components/auth/ChangeProfile.vue
+++ client/src/components/auth/ChangeProfile.vue
@@ -128,9 +128,9 @@ const updateCostumer = () => {
         router.back()
     })
     .catch((error) => {
+      errors.value = error.response.data.errors
         if (error.response.status == 422) {
             toast.error('User was not created due to validation errors!')
-            errors.value = error.response.data.errors
         } else {
             toast.error('User was not created due to unknown server error!')
         }
